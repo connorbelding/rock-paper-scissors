cb633978bb1ed799363d8676c01031c0bec00f33
connorbelding <the_connn@hotmail.com>
2022-09-25 19:20:26 -0400

    Added roundsPlayed variable, adjusted determineResult function

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const results = {
+let roundsPlayed = 0;
+
+let results = {
   wins: 0,
   losses: 0,
   draws: 0,
@@ -6,27 +8,43 @@ const results = {
 
 function getComputerChoice() {
   const randNum = Math.random();
-  if (randNum <= 0.33) return "Rock";
-  if (randNum > 0.33 && randNum <= 0.66) return "Paper";
-  return "Scissors";
+  if (randNum <= 0.33) return "rock";
+  if (randNum > 0.33 && randNum <= 0.66) return "paper";
+  return "scissors";
 }
 
-function determineResult(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) return "draw";
-
-  if (playerSelection === "rock") {
-    if (computerSelection === "scissors") return "win";
-    if (computerSelection === "paper") return "lose";
+function determineResult(playerChoice, computerChoice) {
+  console.log(`${playerChoice} (you) vs ${computerChoice} (cpu)`);
+  if (playerChoice === computerChoice) {
+    results = { ...results, draws: results.draws + 1 };
   }
 
-  if (playerSelection === "paper") {
-    if (computerSelection === "scissors") return "lose";
-    if (computerSelection === "rock") return "win";
+  if (playerChoice === "rock") {
+    if (computerChoice === "scissors") {
+      results = { ...results, wins: results.wins + 1 };
+    }
+    if (computerChoice === "paper") {
+      results = { ...results, losses: results.losses + 1 };
+    }
   }
 
-  if (playerSelection === "scissors") {
-    if (computerSelection === "rock") return "lose";
-    if (computerSelection === "paper") return "win";
+  if (playerChoice === "paper") {
+    if (computerChoice === "rock") {
+      results = { ...results, wins: results.wins + 1 };
+    }
+    if (computerChoice === "scissors") {
+      results = { ...results, losses: results.losses + 1 };
+    }
   }
+
+  if (playerChoice === "scissors") {
+    if (computerChoice === "paper") {
+      results = { ...results, wins: results.wins + 1 };
+    }
+    if (computerChoice === "rock") {
+      results = { ...results, losses: results.losses + 1 };
+    }
+  }
+  console.log(results);
 }
 
