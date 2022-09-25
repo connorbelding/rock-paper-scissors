a780c58e0d3876bfb710a5700d6ccec87c3422ff
connorbelding <the_connn@hotmail.com>
2022-09-25 19:00:25 -0400

    Added results object, added dermineResult function

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
+const results = {
+  wins: 0,
+  losses: 0,
+  draws: 0,
+};
+
 function getComputerChoice() {
   const randNum = Math.random();
   if (randNum <= 0.33) return "Rock";
   if (randNum > 0.33 && randNum <= 0.66) return "Paper";
   return "Scissors";
 }
+
+function determineResult(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) return "draw";
+
+  if (playerSelection === "rock") {
+    if (computerSelection === "scissors") return "win";
+    if (computerSelection === "paper") return "lose";
+  }
+
+  if (playerSelection === "paper") {
+    if (computerSelection === "scissors") return "lose";
+    if (computerSelection === "rock") return "win";
+  }
+
+  if (playerSelection === "scissors") {
+    if (computerSelection === "rock") return "lose";
+    if (computerSelection === "paper") return "win";
+  }
+}
+
