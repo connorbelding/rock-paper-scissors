d1f75b548e5dd4445c481363ffef6885dbd95732
connorbelding <the_connn@hotmail.com>
2022-09-25 17:50:05 -0400

    Added getComputerChoice function

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("bitch");
+function getComputerChoice() {
+  const randNum = Math.random();
+  if (randNum <= 0.33) return "Rock";
+  if (randNum > 0.33 && randNum <= 0.66) return "Paper";
+  return "Scissors";
+}
