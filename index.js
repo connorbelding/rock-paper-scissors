7d07f0af78c0463ebb63099b1285db8e8d3aa855
connorbelding <the_connn@hotmail.com>
2022-09-25 19:30:37 -0400

    Added playRound function

--- index.js
+++ index.js
@@ -14,37 +14,66 @@ function getComputerChoice() {
 }
 
 function determineResult(playerChoice, computerChoice) {
-  console.log(`${playerChoice} (you) vs ${computerChoice} (cpu)`);
+  let message;
   if (playerChoice === computerChoice) {
     results = { ...results, draws: results.draws + 1 };
+    message = `Draw!  ${playerChoice} matches ${computerChoice}.`;
   }
 
   if (playerChoice === "rock") {
     if (computerChoice === "scissors") {
       results = { ...results, wins: results.wins + 1 };
+      message = `You win!  ${playerChoice} beats ${computerChoice}.`;
     }
     if (computerChoice === "paper") {
       results = { ...results, losses: results.losses + 1 };
+      message = `You lose!  ${playerChoice} loses to ${computerChoice}.`;
     }
   }
 
   if (playerChoice === "paper") {
     if (computerChoice === "rock") {
       results = { ...results, wins: results.wins + 1 };
+      message = `You win!  ${playerChoice} beats ${computerChoice}.`;
     }
     if (computerChoice === "scissors") {
       results = { ...results, losses: results.losses + 1 };
+      message = `You lose!  ${playerChoice} loses to ${computerChoice}.`;
     }
   }
 
   if (playerChoice === "scissors") {
     if (computerChoice === "paper") {
       results = { ...results, wins: results.wins + 1 };
+      message = `You win!  ${playerChoice} beats ${computerChoice}.`;
     }
     if (computerChoice === "rock") {
       results = { ...results, losses: results.losses + 1 };
+      message = `You lose!  ${playerChoice} loses to ${computerChoice}.`;
     }
   }
-  console.log(results);
+  return message;
 }
 
+function playRound() {
+  const computerChoice = getComputerChoice();
+  const playerChoice = prompt(
+    `Choose 'rock', 'paper', or 'scissors'.`
+  ).toLowerCase();
+
+  if (
+    playerChoice !== "rock" &&
+    playerChoice !== "paper" &&
+    playerChoice !== "scissors"
+  ) {
+    alert(`Answer must be 'rock', 'paper', or 'scissors'.`);
+    playRound();
+  } else {
+    const result = determineResult(playerChoice, computerChoice);
+    console.log(result);
+    roundsPlayed++;
+    if (roundsPlayed < 5) playRound();
+  }
+}
+
+playRound();
